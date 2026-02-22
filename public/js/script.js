bdf100c645495b25480965100c28896c9420e9a0
solitario001 <messias.bleach@hotmail.com>
2026-02-21 23:46:16 -0300

    feat: implementa IIFE para encapsulamento e proteção de escopo

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,131 @@
-const lista = document.querySelector('#corpo-tabela');
+(function() {
 
-let idatual = null;
+ const lista = document.querySelector('#corpo-tabela');
+ const btnBuscar = document.getElementById('btnBuscar');
+ const form = document.getElementById("meuFormulario");
+ const form2 = document.getElementById("meuFormulario2");
+ const btnRegister = document.getElementById('register');
+ const btnCadastro = document.getElementById('Cadastro');
+    
+ let estaCadastrando = false;
+ let estaEditando = false;
+ let idatual = null;
+
+
+
+ if (btnCadastro) {
+     btnCadastro.addEventListener('click', () => {
+     
+     
+        
+        
+        if (estaEditando ) {
+           alert('Ediçao em andamento 55');
+           return;
+       } 
+        
+       
+        estaCadastrando = true;
+
+        bloquearEdicao(true);
+
+        const collapse = document.getElementById('collapseExample');
+
+        instanciaBS = bootstrap.Collapse.getOrCreateInstance(collapse);
+
+        instanciaBS.show();
+        
+        
+
+     });
+     
+ }
+
+
+
+
+
+ if (btnBuscar) {
+    
+    btnBuscar.addEventListener('click', () => {
+      
+       if (estaEditando || estaCadastrando) {
+           alert('Ediçao em andamento');
+           return;
+       } 
+
+       realizarBuscaGeral();
+    });
+
+    
+ }
+
+  
+
+    if (form) {
+        
+       if (estaEditando) {
+           alert('Ediçao em andamento');
+           return;
+       } 
+
+       
+
+       form.addEventListener('submit', gravarDados);
+         
+    }
+
+
+
+    if (form2) {
+        
+       if (estaEditando) {
+           alert('Ediçao em andamento');
+           return;
+       } 
+
+       form2.addEventListener('submit', atualizarDados);
+         
+    }
+
+
+
 
 
 function bloquearEdicao(status) {
+    
+    
+    
     // Busca todos os elementos que possuem o atributo data-id
     const botoes = document.querySelectorAll('[data-id]');
+    const search = document.querySelectorAll('#btnBuscar');
+    const register = document.querySelectorAll('#Cadastro')
     
     botoes.forEach(btn => {
         btn.disabled = status;
       
         if(status) {
             btn.style.opacity = "0.5";
+        } else {
+            btn.style.opacity = "1";
+        }
+    });
+
+    search.forEach(btn => {
+        btn.disabled = status;
+      
+        if(status) {
+            btn.style.opacity = "0.7";
+        } else {
+            btn.style.opacity = "1";
+        }
+    });
+
+    register.forEach(btn => {
+        btn.disabled = status;
+      
+        if(status) {
+            btn.style.opacity = "0.7";
         } else {
             btn.style.opacity = "1";
         }
@@ -26,13 +140,31 @@ lista.addEventListener('click', function(event) {
     
     // Se o clique foi em um botão de excluir...
     if (botao) {
+        
+        if (estaEditando) {
+        alert("termine a ediçao first 1");
+        return;
+    }
+        
+        
         const idParaExcluir = botao.dataset.id;
         console.log("ID capturado:", idParaExcluir);
         
-       dell(idParaExcluir);
+        dell(idParaExcluir);
     }
 
     if (botao2) {
+        
+
+        if (estaEditando) {
+            alert("Ediçao em andamento");
+            return;
+        }
+        
+        bloquearEdicao(true);
+
+        estaEditando = true;
+        
         const idSelectEdit = botao2.dataset.id;
         console.log("ID capturado:", idSelectEdit);
         idatual = idSelectEdit;
@@ -48,13 +180,18 @@ lista.addEventListener('click', function(event) {
 
 async function dell (id) {
 
+    if (estaCadastrando) {
+        alert('Cadastro em andamento');
+        return;
+    }
+    
     try {
         
         const response = await fetch('http://localhost:3000/deletar-registro', { 
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ id: id })
-    })
+    });
 
     
     if (response) {
@@ -73,9 +210,16 @@ async function dell (id) {
 
 async function selectAlter(id) {
 
+    
+    if (estaCadastrando) {
+        alert('Cadastro em andamento');
+        return;
+    }
+    
+    
     try {
 
-        bloquearEdicao(true); 
+        
 
         const response = await fetch(`http://localhost:3000/select?busca=` + id);
         
@@ -97,6 +241,7 @@ async function selectAlter(id) {
         
         // 4. Abre com animação suave
         instanciaBS.show();
+
 
 
     } catch (error) {
@@ -176,11 +321,14 @@ async function realizarBuscaGeral() {
     } catch (error) {
         console.error('Erro na busca:', error);
     }
+
+
+  
+
+
+
+
 }
-
-
-
-
 
 
 
@@ -189,14 +337,24 @@ async function realizarBuscaGeral() {
 
 async function gravarDados(event) {
     
-    event.preventDefault();  // <--- OBRIGATÓRIO para não recarregar a página
+    
+    
+  
+  
+  
+    event.preventDefault(event);  // <--- OBRIGATÓRIO para não recarregar a página
     document.getElementById('placa').addEventListener('input', (e) => {
     e.target.style.borderColor = ''; // Limpa o vermelho quando ele digita
 });
 
- 
+    if (estaEditando) {
+        alert("termine a edição first 2");
+        event.target.reset();
+        return;
+    }
 
-          
+        
+
         
     // 1. Coleta os 4 inputs
     const dados = {
@@ -241,13 +399,36 @@ async function gravarDados(event) {
          event.target.reset();
         }
     
+        bloquearEdicao(false);
         
+        const elementoCollapse =document.getElementById("collapseExample");
         
+        const instanciaBS = bootstrap.Collapse.getOrCreateInstance(elementoCollapse);
     
+
+        instanciaBS.hide();
+
+        event.target.reset();
+
+        estaCadastrando = false;
+
+
       } catch (error) {
         console.error('Erro ao gravar:', error);
     }
+   
+
+
+   
+
+
 }
+
+    
+
+
+
+
 
 
 
@@ -324,8 +505,13 @@ async function atualizarDados(event) {
         bloquearEdicao(false);
 
         realizarBuscaGeral();
+
+        estaEditando = false;
     
       } catch (error) {
         console.error('Erro ao gravar:', error);
     }
 }
+
+
+})();
